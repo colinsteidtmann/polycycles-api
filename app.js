4feb01faf67532a01c2e038eb9b19be6c2e30a47
colinsteidtmann <colinsteidtmann@yahoo.com>
2021-06-16 17:31:37 -0700

    make it better

--- app.js
+++ app.js
@@ -7,12 +7,8 @@ var cors = require("cors");
 
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var testAPIRouter = require("./routes/testAPI"); // require
 var sendMail = require("./routes/sendMail"); // require
-var sendInfluencerApplication = require("./routes/sendInfluencerApplication"); // require
-var sendBrandApplication = require("./routes/sendBrandApplication"); // require
-var getYoutubeViews = require("./routes/getYoutubeViews"); // require
+var sendSubscribe = require("./routes/sendSubscribe"); // require
 
 var app = express();
 
@@ -28,12 +24,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use("/testAPI", testAPIRouter);
 app.use("/sendMail", sendMail);
-app.use("/sendInfluencerApplication", sendInfluencerApplication);
-app.use("/sendBrandApplication", sendBrandApplication);
-app.use("/getYoutubeViews", getYoutubeViews);
+app.use("/sendSubscribe", sendSubscribe);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
